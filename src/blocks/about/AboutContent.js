8dfdcff08a1555e27c2ba02865bf6d7851810fec
Bianca Read <bianca.read.write@gmail.com>
2021-11-15 21:58:45 -0500

    rearrange blocks;

--- src/blocks/about/AboutContent.js
+++ src/blocks/about/AboutContent.js
@@ -3,6 +3,23 @@ import React from "react";
 const AboutContent = () => {
   return (
     <>
+
+<section id="contact-me" className="block spacer p-top-xl">
+        <div className="row gutter-width-md with-pb-sm">
+          <div className="col-sm-8 col-md-8 col-lg-7 col-xl-7 align-self-center">
+            <div className="title">
+              <h2>COVID-19 Safety Measures</h2>
+            </div>
+
+            <div className="description max-width-1">
+              <p>
+                To ensure the wellbeing of all our guests, we will follow state
+                and local guidance for COVID-19 safety measures.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <section id="contact-me" className="block spacer p-top-xl">
         <div className="row gutter-width-md with-pb-md">
           <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 align-self-center">
@@ -82,23 +99,6 @@ const AboutContent = () => {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="contact-me" className="block spacer p-top-xl">
-        <div className="row gutter-width-md with-pb-sm">
-          <div className="col-sm-8 col-md-8 col-lg-7 col-xl-7 align-self-center">
-            <div className="title">
-              <h2>COVID-19 Safety Measures</h2>
-            </div>
-
-            <div className="description max-width-1">
-              <p>
-                To ensure the wellbeing of all our guests, we will follow state
-                and local guidance for COVID-19 safety measures.
-              </p>
             </div>
           </div>
         </div>
